e85bda79ba8a339652341cdbe4a544dd2669fe27
_tvd <97203853+TalhaAtif@users.noreply.github.com>
2025-09-08 16:45:03 -0400

    Minimal final draft

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize)
 /** @type {HTMLSpanElement} */
 const input = document.querySelector(".textEdit");
 /** @type {HTMLDivElement} */
-const textDiv = document.querySelector(".inputMain");
+const textDiv = document.getElementById("mainInputBox");
+/** @type {HTMLInputElement} */
+const setPass = document.getElementById("setPassword");
 
 const placeholder = "Click here to start..."
-const password = "testing";
+let password = "sample";
 
 input.style.minWidth = textDiv.offsetWidth - 4 * fontSize + 'px';
 
@@ -16,7 +18,10 @@ let time = 0;
 /** @type {HTMLSpanElement} */
 const timerBox = document.getElementById("timerVisual");
 
-
+setPass.addEventListener('input', ()=> {
+    password = setPass.value;
+    console.log(password);
+})
 input.addEventListener('focus', () => {
 
     if (input.textContent === placeholder) {
@@ -33,44 +38,61 @@ input.addEventListener('blur', () => {
 
         timerMode(false);
         time = 0;
+        timerBox.textContent = "";
+
+    }
+})
+
+document.addEventListener("keydown", (pressed) => {
+    if (pressed.key === "Tab") {
+        pressed.preventDefault();
+        resetInput();
+        timerMode(false);
+        time = 0;
+        timerBox.textContent = "";
     }
 })
 
 input.addEventListener('input', () => {
 
-    timerMode(true);
-    /*Changes stuff in the span to dots */
-    const inputDisplay = input.textContent;
-
-    /* Save real input */
-    if (currentTyped.length < inputDisplay.length) {
-        currentTyped += inputDisplay[inputDisplay.length - 1];
-    }
-    else if (currentTyped.length > inputDisplay.length) {
-        currentTyped = currentTyped.slice(0,inputDisplay.length);
-    }
-    console.log("Current typed: " + currentTyped);
-
-
-    input.textContent = "•".repeat(input.textContent.length);
-    input.style.color = 'rgba(244, 240, 0, 1)';
-    if (currentTyped.slice(0, currentTyped.length) !== password.slice(0,currentTyped.length)) {
-        input.style.color = 'red';
-    }
-    if (currentTyped === password) {
+    if (input.textContent.trim() === '') {
+        time = 0;
         timerMode(false);
-        input.style.color = 'rgb(0,255,0)';
     }
+    else {
+        timerMode(true);
+        /*Changes stuff in the span to dots */
+        const inputDisplay = input.textContent;
 
-    /* Cursor Positioning Section */
-    const range = document.createRange();
-    const selected = window.getSelection();
+        /* Save real input */
+        if (currentTyped.length < inputDisplay.length) {
+            currentTyped += inputDisplay[inputDisplay.length - 1];
+        }
+        else if (currentTyped.length > inputDisplay.length) {
+            currentTyped = currentTyped.slice(0, inputDisplay.length);
+        }
 
-    range.selectNodeContents(input);
-    range.collapse(false);
 
-    selected.removeAllRanges();
-    selected.addRange(range);
+        input.textContent = "•".repeat(input.textContent.length);
+        input.style.color = 'rgba(244, 240, 0, 1)';
+        if (currentTyped.slice(0, currentTyped.length) !== password.slice(0, currentTyped.length)) {
+            input.style.color = 'red';
+        }
+        if (currentTyped === password) {
+            timerMode(false);
+            input.style.color = 'rgb(0,255,0)';
+        }
+
+        /* Cursor Positioning Section */
+        const range = document.createRange();
+        const selected = window.getSelection();
+
+        range.selectNodeContents(input);
+        range.collapse(false);
+
+        selected.removeAllRanges();
+        selected.addRange(range);
+    }
 })
 
 function timerMode(on) {
@@ -79,7 +101,7 @@ function timerMode(on) {
         time = 0;
         timer = setInterval(() => {
             time++;
-            let numberVal = (time/100).toFixed(2);
+            let numberVal = (time / 100).toFixed(2);
             timerBox.textContent = numberVal;
         }, 10);
     }
@@ -89,4 +111,14 @@ function timerMode(on) {
             timer = null;
         }
     }
+}
+
+function resetInput() {
+    input.textContent = "";
+    currentTyped = "";
+}
+
+function settings() {
+    var menu = document.getElementById("mySettings");
+    menu.classList.toggle("show");
 }
