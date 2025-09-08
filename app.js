c4c34be5aa7981108ac18a0d2db64cda4148e939
_tvd <talha.atif.89@gmail.com>
2025-09-07 22:47:20 -0400

    Added placeholder system

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize)
 const input = document.querySelector(".textEdit");
 /** @type {HTMLDivElement} */
 const textDiv = document.querySelector(".inputMain");
+const placeholder = "Click here to start..."
 
 input.style.minWidth = textDiv.offsetWidth - 4 * fontSize + 'px';
 
-input.addEventListener('input', () => {
-    if (input.value.length > 0) {
-        input.style.fontSize = '32px';
+input.addEventListener('focus', () => {
+    if (input.textContent === placeholder) {
+        input.textContent = '';
     } 
-    else {
-        input.style.fontSize = '1rem';
+})
+
+input.addEventListener('blur', () => {
+    if (input.textContent.trim() === '') {
+        input.textContent = "Click here to start...";
     }
 })
